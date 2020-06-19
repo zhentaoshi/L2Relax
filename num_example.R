e3ee50049d9a5c57a3af1315b0e4423fe1a7abae
Zhan Gao <zhangao@usc.edu>
2020-06-18 17:31:56 -0700

    add rmosek version of rmosek.

--- num_example.R
+++ num_example.R
@@ -1,6 +1,3 @@
-
-
-
 rm( list = ls() )
 source("opt_func.R")
 
--- opt_func.R
+++ opt_func.R
@@ -1,10 +1,7 @@
 library(CVXR)
 
 
-
-
 # CVX L2-relaxation primal problem
-
 rL2_primal <- function(Sigma, tau = 0) {
   N <- nrow(Sigma)
   w_gamma <- Variable(N + 1)
@@ -17,15 +14,46 @@ rL2_primal <- function(Sigma, tau = 0) {
     Sigma %*% w + gamm <= tau,
     -Sigma %*% w - gamm <= tau
   )
-
+  
   problem <- Problem(objective, constraints)
   result <- solve(problem, solver = "ECOS_BB")
   w_hat <- result$getValue(w_gamma)[1:N]
-
+  
   return(w_hat)
 }
 
-
+rL2_primal_mosek <-  function(Sigma, tau, tol = 1e-7){
+  N <- nrow(Sigma)
+  # variable order: w_1, w_2, ..., w_N, gamma, t, s, r
+  
+  prob = list(sense = "min")
+  prob$dparam <- list(INTPNT_CO_TOL_REL_GAP = tol)
+  
+  prob$c = c( rep(0, N+1), 1/2, rep(0,2) )
+  
+  A_1 = rbind(c(rep(1, N), 0),
+              cbind(Sigma, rep(1, N)))
+  A_2 = rbind( c(1/2, -1, 0), c(1/2, 0, -1) )
+  A = Matrix::bdiag(A_1, A_2)
+  prob$A = as(A, "CsparseMatrix")
+  prob$bc = rbind(blc = c(1, tau * rep(1, N), 1 / 2,-1 / 2),
+                  buc = c(1,-tau * rep(1, N), 1 / 2,-1 / 2))
+  prob$bx = rbind(blx = c(rep(-Inf, N + 1), 0, rep(-Inf, 2)),
+                  bux = rep(Inf, K + 4))
+  
+  prob$cones = matrix(list("QUAD", c(N + 4, 1:N, N + 3)))
+  rownames(prob$cones) <- c("type", "sub")
+  
+  mosek_out = mosek(prob, opts = list(verbose = 0) );
+  
+  xx = mosek_out$sol$itr$xx
+  w = xx[1:N]
+  gamma = xx[N+1]
+  status = mosek.out$sol$itr$solsta
+  
+  return( list(w = w, gamma = gamma, status = status) )
+  
+}
 
 # CVX L2-relaxation dual problem
 
